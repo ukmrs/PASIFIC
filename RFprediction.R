1dd1f6eabe5a7e413cc1eab46f89b8cdc9a9099c
ukmrs <murias.wstork@gmail.com>
2023-01-05 20:13:15 +0100

    added: pasific python script to run everything

--- RFprediction.R
+++ RFprediction.R
@@ -1,13 +1,18 @@
+#!/usr/bin/env Rscript
 require(randomForest) || install.packages("randomForest", repos="http://cran.r-project.org")
 
 # load the random forest 
-load("rf13-3.RData")
 
 # get out filename
-file <- commandArgs(trailingOnly = TRUE)[1]
+args <- commandArgs(trailingOnly = TRUE)
+
+forestmodel <- args[1]
+outfile <- args[2]
+
+load(forestmodel)
 
 # load user's data
-userdata = read.delim(file, header = FALSE, sep="\t", col.names = c("ID", "seq", 
+userdata = read.delim(outfile, header = FALSE, sep="\t", col.names = c("ID", "seq", 
                                                                       "TM", "TM.seq", "start34", "len34", "end34", "G34", "G34.len", "Gfold34", "Gfold34.len", "freq_MFE34", "diversity34", "avg_prob34", "avg_sl34_prob", "mul_prob34", "mul_sl34_prob", 
                                                                       "AT", "AT.seq", "start23", "len23", "end23", "G23", "G23.len", "Gfold23", "Gfold23.len", "freq_MFE23", "diversity23", "avg_prob23", "avg_sl23_prob", "mul_prob23", "mul_sl23_prob", 
                                                                       "P1", "P1.seq", "start", "len", "end", "GP1", "GP1.len", 
@@ -33,4 +38,4 @@ submit = data.frame(ID = userdata$ID,
                     TMfold_free_energy = userdata$Gfold34,
                     ATfold = userdata$ATfold,
                     ATfold_free_energy = userdata$Gfold23)
-write.csv(submit, file = paste(file, ".prediction.csv", sep=""), row.names = FALSE)
+write.csv(submit, file = paste(outfile, ".prediction.csv", sep=""), row.names = FALSE)
